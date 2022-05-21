1fd96d0c6ecf4ce2d38be092a39f48526596ba88
Kiwoong Kim <gw150311@naver.com>
2022-05-21 16:58:56 +0900

    12100 add comment

--- 백준/src/백준12100/Main.java
+++ 백준/src/백준12100/Main.java
@@ -4,7 +4,10 @@
  * USER : KI-WOONG KIM
  * FROM : https://www.acmicpc.net/problem/12100 (2048 (easy))
  * ALGORITMN : Backtracking, Bruteforcing, Graph traversal, Implementation, Simulation
- * NOTE : 백트래킹, 브루트포스로 모든 경로를 탐색하여 최대값을 찾는다.
+ * NOTE : 탐색할 경우의 수가 4^5로 작으므로 완전탐색을 사용할 수 있다
+ *  	  DFS를 통한 백트래킹. 유망한 자식들만 스택에 삽입
+ *  	  Board를 네 방향으로 swipe하는 과정의 구현이 약간 까다롭지만 2D 배열을 적절히 회전(rotate)하여
+ *  	  swipeRight 함수만으로 나머지 세 함수를 구현할 수 있음 
  */
 
 package 백준12100;
@@ -16,11 +19,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Stack;
 
+
 class Board {
-    int size;
-    int[][] values;
-    int maxValue = -1;
-    int swipeCnt = 0;
+    int size; // 가로 세로 길이
+    int[][] values; // 실제 값을 저장할 배열
+    private int maxValue = -1; // 보드의 최댓값
+    int swipeCnt = 0; // swipe한 횟수
     
     protected Board(int size) {
 	this.size = size;
@@ -34,40 +38,68 @@ class Board {
 	this.swipeCnt = swipeCnt;
     }
     
-    protected ArrayList<Board> createchildren() {
-	ArrayList<Board> children = new ArrayList<Board>();
-	children.add(this.swipeRight());
-	children.add(this.swipeLeft());
-	children.add(this.swipeUp());
-	children.add(this.swipeDown());
-	return children;
+    /**
+     * 보드에서 최댓값을 찾아서 갱신하고 리턴한다
+     * 초기보드의
+     * @return 보드의 최댓값
+     */
+    protected int maxValue() {
+	int max = -1;
+	for (int i = 0; i < size; i++) {
+	    for (int j = 0; j < size; j++) {
+		if (values[i][j] > max)
+		    max = values[i][j];
+	    }
+	}
+	this.maxValue = max;
+	return this.maxValue;
+    }
+    /**
+     * 오른쪽, 왼쪽, 위쪽, 아래쪽으로 swipe하여 만들어지는 다음 상태의 보드들을 리턴함
+     * @return
+     */
+    protected ArrayList<Board> createNextStates() {
+	ArrayList<Board> nextStates = new ArrayList<Board>();
+	nextStates.add(this.swipeRight());
+	nextStates.add(this.swipeLeft());
+	nextStates.add(this.swipeUp());
+	nextStates.add(this.swipeDown());
+	return nextStates;
     }
     
+    /**
+     * 보드를 오른쪽으로 스윕한다
+     * @return 보드를 오른쪽으로 스윕하여 만들어진 새로운 보드
+     */
     private Board swipeRight() {
-
-	Board b = new Board(this.size, null, this.maxValue, this.swipeCnt + 1);
+	Board b = new Board(this.size, null, this.maxValue(), this.swipeCnt + 1);
+	
+	// 현재 보드의 값을 카피
 	int[][] deepCopy = new int[b.size][b.size];
 	for (int i = 0; i < b.size; ++i) {
 	    deepCopy[i] = Arrays.copyOf(this.values[i], b.size);
 	}
 	b.values = deepCopy;
 	
+	// Swipe
+	// 오른쪽으로 스윕하므로 각 row별로 처리 (서로 다른 row끼리는 상호작용 없음)
 	for (int row = 0; row < b.size; ++row) {
+	    // 한번의 스윕에서 하나의 블럭은 한번만 병합될 수 있음
+	    // i-th column의 블럭이 이미 병합된 블럭인지 체크
 	    boolean[] mergedBlock = new boolean[b.size];
 	    int i = b.size - 1;
 	    for (int col = b.size - 1; col >= 0; --col) {
 		if (b.values[row][col] != 0) {  // 0이 아니면 오른쪽으로 스윕
 		    // 같은 수 두개 만나면 합쳐짐
 		    if(i < b.size - 1 && (b.values[row][i+1] == b.values[row][col])
-			    		&& !mergedBlock[i+1]) { // 합쳐져서 만들어진 블럭은 다시 합칠수 없음
+			    		&& !mergedBlock[i+1]) {
 			b.values[row][i+1] += b.values[row][col];
-			b.values[row][col] = 0;
+			b.values[row][col] = 0; // 이전에 있던 자리는 빈칸 처리
 			// 머지로 만들어진 블럭값이 스윕 이전의 max보다 크면 업데이트
 			if (b.values[row][i+1] > b.maxValue) {
 			    b.maxValue = b.values[row][i+1]; 
 			}
 			mergedBlock[i+1] = true;
-			
 		    } else {
 			b.values[row][i] = b.values[row][col];
 			if (i != col) {
@@ -81,8 +113,12 @@ class Board {
 	return b;
     }
     
+    /**
+     * 각 row를 reverse - 오른쪽으로 스윕 - 각 row를 reverse
+     * @return 보드를 왼쪽으로 스윕하여 만들어진 새로운 보드
+     */
     private Board swipeLeft() {
-	Board b = new Board(this.size, this.values, this.maxValue, this.swipeCnt);
+	Board b = new Board(this.size, this.values, this.maxValue(), this.swipeCnt);
 	int[][] temp = new int[b.size][b.size];
 	for (int row = 0; row < b.size; ++row) {
 	    for (int col = 0; col < b.size; ++col) {
@@ -103,8 +139,12 @@ class Board {
 	return b;
     }
     
+    /**
+     * 시계방향 회전 - 오른쪽으로 스윕 - 반시계 방향으로 회전
+     * @return 보드를 위쪽으로 스윕하여 만들어진 새로운 보드
+     */
     private Board swipeUp() {
-	Board b = new Board(this.size, this.values, this.maxValue, this.swipeCnt);
+	Board b = new Board(this.size, this.values, this.maxValue(), this.swipeCnt);
 	int[][] temp = new int[b.size][b.size];
 	for (int row = 0; row < b.size; ++row) {
 	    for (int col = 0; col < b.size; ++col) {
@@ -124,8 +164,12 @@ class Board {
 	return b;
     }
     
+    /**
+     * 반시계방향 회전 - 오른쪽으로 스윕 - 시계 방향으로 회전
+     * @return 보드를 아래쪽으로 스윕하여 만들어진 새로운 보드
+     */
     private Board swipeDown() {
-	Board b = new Board(this.size, this.values, this.maxValue, this.swipeCnt);
+	Board b = new Board(this.size, this.values, this.maxValue(), this.swipeCnt);
 	int[][] temp = new int[b.size][b.size];
 	for (int row = 0; row < b.size; ++row) {
 	    for (int col = 0; col < b.size; ++col) {
@@ -144,20 +188,14 @@ class Board {
 	b.values = temp2;
 	return b;
     }
-   
-    
-    protected int findMax() {
-	int max = -1;
-	for (int i = 0; i < size; i++) {
-	    for (int j = 0; j < size; j++) {
-		if (values[i][j] > max)
-		    max = values[i][j];
-	    }
-	}
-	this.maxValue = max;
-	return this.maxValue;
-    }
+  
 
+    /**
+     * another의 보드의 상태가 이 보드와 같으면 true 리턴
+     * 즉 보드의 크기가 같고 모든 인덱스의 값이 같을 때에만 true 
+     * @param another 비교할 보드
+     * @return
+     */
     protected boolean equals(Board another) {
 	if (this.size != another.size)
 	    return false;
@@ -172,6 +210,10 @@ class Board {
 	return true;
     }
     
+    /**
+     * 보드의 상태를 출력
+     * 각 인덱스의 값, 보드의 최댓값, 변경 횟수(swipeCnt)
+     */
     protected void print() {
 	for (int i = 0; i < size; i++) {
 	    for (int j = 0; j < size; j++) {
@@ -187,21 +229,22 @@ class Board {
 
 class Solution {
     protected int solution(Board initialBoard) {
-	int answer = initialBoard.maxValue;
-	int maxAnswer = 32 * initialBoard.maxValue;
+	int answer = initialBoard.maxValue();
+	int maxAnswer = 32 * answer;
 	Stack<Board> stack = new Stack<Board>();
 	stack.push(initialBoard);
 	Board b;
 	while (!stack.isEmpty()) {
 	    b = stack.pop();
-	    if (b.maxValue > answer) {
-		answer = b.maxValue;
+	    int newMax = b.maxValue();
+	    if (newMax > answer) {
+		answer = newMax;
 		if (answer == maxAnswer) {
 		    return answer;
 		}
 	    }
 	    if (b.swipeCnt < 5) {
-		ArrayList<Board> nextStates = b.createchildren();
+		ArrayList<Board> nextStates = b.createNextStates();
 		for (Board nextState : nextStates) {
 		    if (!nextState.equals(b)) {
 		    	stack.push(nextState);
@@ -220,7 +263,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
 	input();
 	System.out.println((new Solution()).solution(board));
-
     }
 
     private static void input() throws IOException {
@@ -232,7 +274,5 @@ public class Main {
 	    int[] arr = Arrays.stream(line).mapToInt(Integer::parseInt).toArray();
 	    board.values[i] = arr;
 	}
-	board.findMax();
     }
-
 }
